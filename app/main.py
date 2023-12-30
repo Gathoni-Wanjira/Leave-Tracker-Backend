ad46fda53b01fbf3e2b0c2da0459b1b618ee4899
GATHONI-WANJIRU <janetgathoni910@gmail.com>
2023-12-30 18:11:27 +0300

    Complete mail server with routing & template

--- app/main.py
+++ app/main.py
@@ -1,30 +1,65 @@
 # importing libraries 
-from flask import Flask 
+from flask import Flask ,request
 from flask_mail import Mail, Message 
+from flask_cors import CORS
+
+
 
 app = Flask(__name__) 
-mail = Mail(app) # instantiate the mail class 
+CORS(app)
+
 
 # configuration of mail 
-app.config['MAIL_SERVER']='wanjirajgathoni@gmail.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'janetgathoni97@gmail.com'
-app.config['MAIL_PASSWORD'] = 'JA11NE8/GA'
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-mail = Mail(app) 
+# Mail server to link the email between the sender and hr 
+app.config['MAIL_SERVER']='sandbox.smtp.mailtrap.io'
+app.config['MAIL_PORT'] = 2525
+app.config['MAIL_USERNAME'] = '921198b40eda6e'
+app.config['MAIL_PASSWORD'] = 'c19ea17cc955ea'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+mail = Mail(app) # instantiate the mail class  
 
 # message object mapped to a particular URL ‘/’ 
-@app.route("/") 
-def index(): 
-msg = Message( 
-				'Hello', 
-				sender ='janetgathoni97@gmail.com', 
-				recipients = ['janetgathoni910@gmail.com'] 
-			) 
-msg.body = 'Hello Flask message sent from Flask-Mail'
-mail.send(msg) 
-return 'Sent'
+
+@app.route("/", methods = ['POST'])
+def index():
+    data = request.get_json() 
+    full_name = data["fullName"]
+    department = data["department"]
+    reason =data["reason"]
+    start_date = data["startDate"]
+    end_date =data["endDate"]
+    email_address = data["email"]
+
+
+    leave_request_email = f"""
+    
+    Dear HR,
+
+    I hope this message finds you well. I would like to request leave from the {department} department for the following reasons:
+
+    - Reason for Leave: {reason}
+    - Start Date: {start_date}
+    - End Date: {end_date}
+
+    If there are any specific procedures or forms that need to be completed for the leave request, please let me know, and I will promptly take care of them.
+
+    Your understanding and support regarding this matter are highly appreciated.
+
+    Regards,
+
+    {full_name}
+    {email_address}
+    """
+
+    print(leave_request_email)
+
+# recipients = hr's image
+# Sender = internal email address
+    msg = Message(f'Subject: Leave Request - {full_name}', sender =   'peter@mailtrap.io', recipients = ['paul@mailtrap.io'])
+    msg.body = leave_request_email
+    mail.send(msg)
+    return "Message sent!"
 
 if __name__ == '__main__': 
-app.run(debug = True) 
+    app.run(debug = True) 
